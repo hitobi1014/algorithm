a572f279ec6df1e9dbd39fc5c4b7be7f74a79fdd
minjun <hitobi1014@gmail.com>
2022-05-14 22:52:01 +0900

    6.3 문제 소풍 first commit

---
+++ src/main/java/Chapter03_6/PicnicPage155.java
@@ -0,0 +1,18 @@
+package Chapter03_6;
+
+public class PicnicPage155 {
+    public static void main(String[] args) {
+        /* 입력 첫 줄에는 테스트 케이스의 수 C(C<= 50)
+        *
+        * ## 첫 줄에는 학생의 수 n(2 <= n <= 10)
+        *    ,        친구 쌍의 수 m(0<= m <= n(n-1)/2)
+        *
+        * # 번호는 모두 0부터 n-1 사이의 정수
+        * # 같은 쌍은 입력에 두 번 주어지지 않음
+        * # 학생들의 수는 짝수
+        * */
+
+
+    }
+}
+
