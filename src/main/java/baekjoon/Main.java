37243873d6d942ecf32ad2390ee3eb9667c0364f
Minjun <hitobi1014@gmail.com>
2022-11-21 12:34:26 +0900

    배열 - 과제안내신분?

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -3,22 +3,21 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 30명 중 2명이 과제 제출x 번호는 1~30번 .. 안낸사람 찾기
+        // input : 28 line , 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = 0;
-        int line = 0;
-        int num = 0;
+        ArrayList<Integer> student = new ArrayList<>();
 
-        for (int i = 0; i < 9; i++) {
-            num = Integer.parseInt(br.readLine());
-            if (max < num) {
-                max = num;
-                line++;
-            }
+        for (int i = 1; i <= 30; i++) {
+            student.add(i);
         }
 
-        System.out.print(max + "\n" + line);
+        for (int i = 0; i < 30; i++) {
+        }
+        
     }
 }
