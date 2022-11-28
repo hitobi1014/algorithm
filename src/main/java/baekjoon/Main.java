39377e007e4ad262b1298c616d5c8c3426e15dd7
Minjun <hitobi1014@gmail.com>
2022-11-28 12:54:03 +0900

    서로 다른 나머지 구하기

--- src/main/java/baekjoon/Main.java
+++ src/main/java/baekjoon/Main.java
@@ -3,21 +3,28 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 30명 중 2명이 과제 제출x 번호는 1~30번 .. 안낸사람 찾기
-        // input : 28 line , 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> student = new ArrayList<>();
+        int[] numArr = new int[10]; // 나머지 저장 배열
+        int count = 0;
+        boolean equal;
 
-        for (int i = 1; i <= 30; i++) {
-            student.add(i);
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = Integer.parseInt(br.readLine()) % 42;
         }
 
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < numArr.length; i++) {
+            equal = false;
+            for (int j = i + 1; j < numArr.length; j++) {
+                if (numArr[i] == numArr[j]) {
+                    equal = true;
+                    break;
+                }
+            }
+            if (!equal) count++;
         }
-        
+
     }
 }
