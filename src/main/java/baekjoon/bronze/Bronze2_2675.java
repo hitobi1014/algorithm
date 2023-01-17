450d78d3e26f898a22a388f5ff470002d5d2268a
choi min jun <hitobi1014@gmail.com>
2023-01-17 13:25:49 +0900

    백준 Q.2675 문자열 반복 : StringTokenizer, StringBuilder 사용방법도 있음

---
+++ src/main/java/baekjoon/bronze/Bronze2_2675.java
@@ -0,0 +1,34 @@
+package baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Q.
+ * 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오.
+ * 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+ * QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+ *
+ * Input.
+ * 첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
+ *
+ * Output.
+ * 각 테스트 케이스에 대해 P를 출력한다.
+ */
+public class Bronze2_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCaseCnt = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCaseCnt; i++) {
+            String[] word = br.readLine().split(" ");
+            for (int j = 0; j < word[1].length(); j++) {
+                for (int k=0; k<Integer.parseInt(word[0]);k++) {
+                    System.out.print(word[1].charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
