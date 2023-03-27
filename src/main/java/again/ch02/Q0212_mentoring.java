1befe10b1581cc7e7b6720644c45f512a4f92c03
minjun <hitobi1014@gmail.com>
2023-03-27 23:05:00 +0900

    인프런 - 멘토링

--- src/main/java/again/ch02/Q0212_mentoring.java
+++ src/main/java/again/ch02/Q0212_mentoring.java
@@ -2,6 +2,7 @@ package again.ch02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * 설명
@@ -30,6 +31,38 @@ import java.io.InputStreamReader;
  */
 public class Q0212_mentoring {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Q0212_mentoring T = new Q0212_mentoring();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, m, arr));
+
+    }
+
+    public int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        if(arr[k][s]==i) pi=s;
+                        if(arr[k][s]==j) pj=s;
+                    }
+                    if(pi<pj) cnt++;
+                }
+                if (cnt == m) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
     }
 }
