8548ab67966469c58995d94cbdf51f56a748aba7
minjun <hitobi1014@gmail.com>
2023-07-25 18:01:22 +0900

    문제 푸는중 유기농 배추

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -4,38 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 0;
-        System.out.println(a/5);
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int M = sc.nextInt();
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+        }
+
     }
 
     public static int solution(int sugar) {
         int answer = 0;
-//        int[] items = {3, 5};//// 테스트
-        int remain = 0;
-
-        remain = sugar % 5;
-        if (remain == 0) {
-            answer = sugar / 5;
-        }
-        if (remain != 0 && remain % 3 == 0) {
-            answer += remain / 3;
-        }
-
-//        if (sugar % 5 == 0) {
-//            remain = sugar % 5;
-//            answer = sugar / 5;
-//            if (remain % 3 == 0) {
-//                answer += remain / 3;
-//            }
-//        }
-//
-//        while (sugar != 0) {
-//            remain = sugar % 5;
-//
-//            if (remain % 3 == 0) {
-//
-//            }
-//        }
 
         return answer;
     }
