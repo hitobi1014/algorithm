0e35ba2e800d7934a7b275778aa612e36b2c5dc7
minjun <hitobi1014@gmail.com>
2023-08-02 17:57:05 +0900

    백준 구간합 푸는중

--- src/main/java/test/백준.java
+++ src/main/java/test/백준.java
@@ -1,12 +1,43 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /**
- * 1244 스위치 켜고끄기
+ * 11660 - 구간 합 구하기 5
  */
 public class 백준 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("D:\\MJ\\src\\main\\java\\test\\text.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N+1][N+1];
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < N+1; j++) {
+                arr[i][j] = arr[i][j-1]+arr[i-1][j]- arr[i-1][j-1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int calc = arr[x2][y2] - arr[x1-1][y2] - arr[x1][y1-1] + arr[x1-1][y1-1];
+            sb.append(calc).append('\n');
+        }
+        System.out.println(sb);
     }
-
 
 }
