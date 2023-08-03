1cbc3d57576aecb7d0197e20f03f67ade04db24b
minjun <hitobi1014@gmail.com>
2023-08-03 18:01:25 +0900

    백준 문제 푸는중

--- src/main/java/test/백준.java
+++ src/main/java/test/백준.java
@@ -3,41 +3,85 @@ package test;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
- * 11660 - 구간 합 구하기 5
+ * 12891 - DNA 비밀번호
  */
 public class 백준 {
+
+    //DNA문자 => A C G T
+    // 부분문자열 문자 개수가 특정 개수 이상이어야 사용 가능
+    static char[] checkDna = {'A', 'C', 'G', 'T'};
+    static char[] cArr;
+    static int[] checkCnt = new int[4];
+    static boolean[] isSelected;
+    static int S,P, answer;
+    static Map<Character, Integer> checkMap = new HashMap<>();
     public static void main(String[] args) throws Exception{
         System.setIn(new FileInputStream("D:\\MJ\\src\\main\\java\\test\\text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N+1][N+1];
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < N+1; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < N+1; j++) {
-                arr[i][j] = arr[i][j-1]+arr[i-1][j]- arr[i-1][j-1] + Integer.parseInt(st.nextToken());
+        // 임의로만든 문자열 길이
+        S = Integer.parseInt(st.nextToken());
+        // 비밀번호로 사용할 문자열 길이
+        P = Integer.parseInt(st.nextToken());
+        String dnaStr = br.readLine();
+        st = new StringTokenizer(br.readLine());
+        cArr = dnaStr.toCharArray();
+
+        for (int i = 0; i < checkDna.length; i++) {
+            checkMap.put(checkDna[i], Integer.parseInt(st.nextToken()));
+        }
+
+        solution();
+        System.out.println(answer);
+
+    }
+
+    private static void solution(){
+        char[] temp = new char[P];
+        int lt=0,rt=P-1;
+        for (int i = 0; i <= P; i++) {
+            char x = cArr[i];
+            if (x == checkDna[0]) checkCnt[0]++;
+            if (x == checkDna[1]) checkCnt[1]++;
+            if (x == checkDna[2]) checkCnt[2]++;
+            if (x == checkDna[3]) checkCnt[3]++;
+        }
+//        static char[] checkDna = {'A', 'C', 'G', 'T'};
+//        static char[] cArr;
+//        static int[] checkCnt = new int[4]; ... 또륵
+
+
+        while (rt-lt <= P) {
+            String str = String.copyValueOf(temp);
+            boolean flag = true;
+
+            for (Map.Entry<Character, Integer> entry : checkMap.entrySet()) {
+                int charCnt = str.length() - str.replace(String.valueOf(entry.getKey()), "").length();
+                if (charCnt < entry.getValue()) {
+                    flag = false;
+                    break;
+                }
             }
+            if (flag) answer++;
+            lt++;
+            rt++;
+
+            if (rt >= cArr.length) break;
+            temp[0] = cArr[lt];
+            temp[P-1] = cArr[rt];
         }
 
+    }
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+    private static boolean checkDna() {
 
-            int calc = arr[x2][y2] - arr[x1-1][y2] - arr[x1][y1-1] + arr[x1-1][y1-1];
-            sb.append(calc).append('\n');
-        }
-        System.out.println(sb);
+        return false;
     }
 
 }
