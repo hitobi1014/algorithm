8ed254d684aa5f8bbfb1246a24d8341b78039148
minjun <hitobi1014@gmail.com>
2023-08-03 18:01:12 +0900

    백준 문제 푸는중

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,21 +1,19 @@
 package test;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sugar = sc.nextInt();
-        if (sugar == 4 || sugar == 7) {
-            System.out.println(-1);
-        } else if (sugar % 5 == 0) {
-            System.out.println((sugar / 5));
-        } else if (sugar % 5 == 1 || sugar % 5 == 3) {
-            System.out.println((sugar / 5) + 1);
-        } else if (sugar % 5 == 2 || sugar % 5 == 4) {
-            System.out.println((sugar / 5) + 2);
-        }
+    public static void main(String[] args) throws IOException {
+        char[] cArr = {'C', 'A', 'B', 'C','C'};
+        System.out.println(String.copyValueOf(cArr));
+        String str = String.copyValueOf(cArr);
+        int cnt = str.length() - str.replace(String.valueOf('C'), "").length();
+        System.out.println(cnt);
+    }
+
+    static void solution(){
 
     }
+
 
 }
