d8a43f1f1dcfa346cd2d78392ee6e52e84603c85
Minjun Choi <66252942+hitobi1014@users.noreply.github.com>
2023-08-07 13:28:38 +0900

    [Gold V] Title: 탑, Time: 852 ms, Memory: 123452 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 2493 - 탑
+ */
+public class Main {
+    static Map<Integer, Integer> map = new HashMap<>();
+    static int N;
+
+    static Map<Character, Integer> checkMap = new HashMap<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            map.put(arr[i], i + 1); // 입력값 key, value => 1~N까지
+        }
+        solution(arr);
+    }
+
+    static void solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[N];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            int x = arr[i];
+
+            while (!stack.isEmpty()) {
+                if (stack.peek() > x) {
+                    answer[i] = map.get(stack.peek());
+                    break;
+                }
+                if (stack.peek() < x) {
+                    stack.pop();
+                }
+            }
+            stack.push(x);
+        }
+        
+        for (int x : answer) {
+            sb.append(x).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+
+}
