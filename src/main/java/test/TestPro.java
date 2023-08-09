7b1859decaa07f06d38f6ab843a7d90f3e0b118e
minjun <hitobi1014@gmail.com>
2023-08-09 17:48:12 +0900

    절댓값 힙 추가

---
+++ src/main/java/test/TestPro.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestPro {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.remove(0);
+        System.out.println();
+
+
+    }
+}
