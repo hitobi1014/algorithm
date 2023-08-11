6c7fe977818152d995f977474e3fed408c26c937
minjun <hitobi1014@gmail.com>
2023-08-11 23:16:42 +0900

    SWEA 상호 배틀필드 리팩토링

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -15,23 +14,27 @@ public class Solution {
     private static char[][] map;
     // 0번idx 탱크 문자정보 1번 idx row 정보,2번 idx col 정보
     private static char[] tankInfo;
-    private static Map<Character, Integer> tankInit = new HashMap<>();
+    private static Map<Character, Integer> tankInit = new HashMap<>(); // 전차 방향 기본 설정
+    private static Map<Character, Character> cmdConnection = new HashMap<>(); // mapping
 
     static {
+        // 방향 <-> 4방 인덱스 매핑
         tankInit.put('^', 0);
         tankInit.put('v', 1);
         tankInit.put('<', 2);
         tankInit.put('>', 3);
-        tankInit.put('U', 0);
-        tankInit.put('D', 1);
-        tankInit.put('L', 2);
-        tankInit.put('R', 3);
+        // 명령어 <-> 방향 매핑
+        cmdConnection.put('U', '^');
+        cmdConnection.put('D', 'v');
+        cmdConnection.put('L', '<');
+        cmdConnection.put('R', '>');
     }
 
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("D:\\00.project\\algorithm\\src\\main\\java\\test\\text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
@@ -52,12 +55,9 @@ public class Solution {
                     map[j][k] = c;
 
                     if (tankInit.containsKey(c)) {
-//                        tankInfo[0] = (char)(tankInit.get(c)+'0');
                         tankInfo[0] = c;
                         tankInfo[1] = (char) (j + '0');
                         tankInfo[2] = (char) (k + '0');
-
-//                        tankStart.put(c, new int[]{j,k});
                     }
 
                 }
@@ -67,7 +67,9 @@ public class Solution {
             int N = Integer.parseInt(br.readLine());
             char[] commands = br.readLine().toCharArray();
 
+            // 명령어에 따라 맵 조작
             processMap(commands);
+
             sb.append("#").append(i).append(' ');
             for (int a = 0; a < map.length; a++) {
                 for (int b = 0; b < map[a].length; b++) {
@@ -77,47 +79,28 @@ public class Solution {
             }
 
         }
-
-
         System.out.println(sb.toString());
     }
 
     private static void processMap(char[] commands) {
-//        char tankDir = tankInfo[0];
-//        int row = tankInfo[1], col = tankInfo[2];
-
         for (char c : commands) {
             if (c == 'S') {
                 shoot();
             } else {
                 moveTank(c);
             }
-
-//            System.out.println("=========" + c + "=========");
-//            for (int i = 0; i < map.length; i++) {
-//                for (int j = 0; j < map[i].length; j++) {
-//                    System.out.print(map[i][j]+"\t");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println("=================================================");
         }
     }
 
-    //    private static void shoot(char tankDir, int row, int col){
     private static void shoot() {
         // * - 벽돌로 만들어진벽은 파괴되고 평지로 변함
         // # - 강철로 만들어진벽은 아무런 변함 없음
-        // 맵 밖에 나갈때까지 진행됨
-//        int idx = tankInit.get(tankDir);
-//        int r = row - dx[idx];
-//        int c = col - dy[idx];
-
         int idx = tankInit.get(tankInfo[0]);
         int r = (tankInfo[1] - '0') + dx[idx];
         int c = (tankInfo[2] - '0') + dy[idx];
 
         while (true) {
+            // 범위가 맵 밖으로 나갈때 까지 진행
             if (r < 0 || r >= map.length || c < 0 || c >= map[0].length) break;
             if (map[r][c] == '#') break;
             if (map[r][c] == '*') {
@@ -127,85 +110,41 @@ public class Solution {
 
             r = r + dx[idx];
             c = c + dy[idx];
-
-            // 평지일때 해당 방향으로 포탄전진
-//            if (map[r][c] == '.' || ) {
-//                r = r + dx[idx];
-//                c = c + dy[idx];
-//                continue;
-//            }
         }
     }
 
-    //    private static void moveTank(char tankDir, int row, int col){
     private static void moveTank(char dir) {
-//        int idx = tankInit.get();
-//        int r = row - dx[idx];
-//        int c = col - dy[idx];
+        char tankDir = cmdConnection.get(dir);
+        int idx = tankInit.get(tankDir);
 
-        int idx = tankInit.get(dir);
+        // 현재 전차 위치에서 move명령에 맞는 방향 머리 돌리기
+        map[tankInfo[1] - '0'][tankInfo[2] - '0'] = tankDir;
+        tankInfo[0] = tankDir;
 
-        switch (dir) {
-            case 'U':
-                map[tankInfo[1] - '0'][tankInfo[2] - '0'] = '^';
-                tankInfo[0] = '^';
-                break;
-            case 'D':
-                map[tankInfo[1] - '0'][tankInfo[2] - '0'] = 'v';
-                tankInfo[0] = 'v';
-                break;
-            case 'L':
-                map[tankInfo[1] - '0'][tankInfo[2] - '0'] = '<';
-                tankInfo[0] = '<';
-                break;
-            case 'R': {
-                map[tankInfo[1] - '0'][tankInfo[2] - '0'] = '>';
-                tankInfo[0] = '>';
-            }
-            break;
-        }
+        int tRow = tankInfo[1] - '0';
+        int tCol = tankInfo[2] - '0';
 
+        int r = tRow + dx[idx];
+        int c = tCol + dy[idx];
 
-        int r = (tankInfo[1] - '0') + dx[idx];
-        int c = (tankInfo[2] - '0') + dy[idx];
-
+        // 맵을 벗어나면 return
         if (r < 0 || r >= map.length || c < 0 || c >= map[0].length) return;
 
+        // 1. 다음 전진 할 방향이 평지이면 해당 칸으로 이동
+        // 2. 이전에 있던 위치는 평지로 매핑
+        // 3. 이동 한 위치 tankInfo에 저장
         if (map[r][c] == '.') {
-            map[tankInfo[1] - '0'][tankInfo[2] - '0'] = '.';
-            switch (dir) {
-                case 'U':
-                    map[r][c] = '^';
-                    tankInfo[0] = '^';
-                    break;
-                case 'D':
-                    map[r][c] = 'v';
-                    tankInfo[0] = 'v';
-                    break;
-                case 'L':
-                    map[r][c] = '<';
-                    tankInfo[0] = '<';
-                    break;
-                case 'R': {
-                    map[r][c] = '>';
-                    tankInfo[0] = '>';
-                }
-                break;
-            }
-//            tankInfo[0] = dir;
+            map[tRow][tCol] = '.';
+            map[r][c] = tankDir;
+            tankInfo[0] = tankDir;
             tankInfo[1] = (char) (r + '0');
             tankInfo[2] = (char) (c + '0');
         }
-
-        ;
-
-        // 평지인경우만 이동
-//        while (true){
-//            if (r < 0 || r >= map.length || c <0 || c >= map.length) break;
-//            if (map[r][c] != '.') break;
-//        }
 
     }
 
 
 }
+
+
+
