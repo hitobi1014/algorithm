e1e4d7e0b0b6f606575c09fdc5c98a2568941e11
choi min jun <hitobi1014@gmail.com>
2023-08-17 20:51:08 +0900

    동승이에게 '참'교육 받은날

--- src/main/java/test/백준.java
+++ src/main/java/test/백준.java
@@ -1,44 +1,64 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class 백준 {
 
+
     private static int n, pow;
-    private static boolean[] arr;
+    private static int[] num = {2, 3, 5, 7};
+    private static String[] n1 = {"1", "3", "7", "9"};
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new boolean[(int) Math.pow(10,n)];
 
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = true;
-        }
-
-        for (int i = 2; i < arr.length; i++) {
-            if(!arr[i]) continue;
-            for (int j = 2*i; j < arr.length; j+=i) {
-                arr[j] = false;
-            }
-        }
-
-        System.out.println();
-        pow = (int) Math.pow(10, n - 1);
-
-        System.out.println();
+        find();
+//        System.out.println(sb.toString());
     }
 
-    private static boolean find(int z, int idx, int cnt) {
+    private static boolean find() {
+        // (1) 맨 앞자리가 2,3,5,7로 시작하는애들
+        // (2) 짝수, 5,7 배수 제외하기
+        // (3) 끝자리가 1,3,7,9 얘네인애들
+        int pow = 4; // 임시 자리수
+        ArrayList<String> listA = new ArrayList<>();
+        listA.add("2");
+        listA.add("3");
+        listA.add("5");
+        listA.add("7");
+        ArrayList<String> listB;
 
-        for (int i = pow; i < arr.length; i++) {
-            if(arr[i] && find(i/10, i)){
-                if ((int)Math.log10(i)+1 == pow) sb.append(i);
+        for (int k = 0; k < n-1; k++) {
+            listB = new ArrayList<>();
+
+            for (int i = 0; i < listA.size(); i++) {
+                String n = listA.get(i);
+
+                for (int j = 0; j < n1.length; j++) {
+                    String str2 = n + n1[j];
+                    if (isPrime(Integer.valueOf(str2))) {
+                        listB.add(str2);
+                    }
+                }
+
             }
+            listA = listB;
         }
 
+        listA.forEach(System.out::println);
+
         return false;
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 3; i < n; i++) {
+            if(n%i==0) return false;
+        }
+        return true;
     }
 
 }
