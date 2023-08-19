d97d4b4f0089c716338d71b4385c2b9a62ab2fa6
choi min jun <hitobi1014@gmail.com>
2023-08-19 17:09:14 +0900

    보물 푸는중

--- src/main/java/test/백준.java
+++ src/main/java/test/백준.java
@@ -1,71 +1,40 @@
 package test;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.StringTokenizer;
-
+import java.util.function.Function;
 
 public class 백준 {
-    static int N, M, V;
-    static List<ArrayList<Integer>> list;
-    static boolean[] visited; // BFS 방문체크
+    static int N, answer;
+    static int[] A, B;
+    static Queue<Integer> queue = new ArrayDeque<>();
     public static void main(String[] args) throws Exception{
-        System.setIn(new FileInputStream("D:\\MJ\\algorithm\\src\\main\\java\\test\\text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<String, Integer> sToNum = Integer::parseInt;
+        N = sToNum.apply(br.readLine());
+        A = new int[N];
+        B = new int[N];
+
+        // A
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-
-        list = new ArrayList<>();
-        for (int i = 0; i <= M; i++) {
-            list.add(new ArrayList<>());
+        for (int i = 0; i < N; i++) {
+            queue.offer(sToNum.apply(st.nextToken()));
         }
+        Arrays.sort(queue.toArray());
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int firstVertext = Integer.parseInt(st.nextToken());
-            int secondVertext = Integer.parseInt(st.nextToken());
-            list.get(firstVertext).add(secondVertext);
-//            list.add(firstVertext, new int[]{firstVertext, secondVertext});
+        // B
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = sToNum.apply(st.nextToken());
         }
-
-        bfs();
-    }
-
-    private static void bfs() {
-        Queue<List> queue = new ArrayDeque<>();
-        List<Integer> answer = new ArrayList<>();
-        visited = new boolean[list.size()];
-        // 1부터시작
-        queue.offer(list.get(1));
-        visited[1] = true;
-        answer.add(list.get(1).get(1));
-
-        while (!queue.isEmpty()) {
-            ArrayList<Integer> getList = (ArrayList<Integer>) queue.poll();
-
-            for (int i = 0; i < getList.size(); i++) {
-                int next = getList.get(i);
-                if(next != 0 && !visited[next]){
-                    answer.add(next);
-                    queue.offer(list.get(next));
-                    visited[next] = true;
-                }
-            }
-        }
-
-
-        answer.forEach(System.out::println);
+        System.out.println();
 
     }
 
-    private static void dfs() {
+    private static void findMinValue() {
 
     }
-
 
 
 }
