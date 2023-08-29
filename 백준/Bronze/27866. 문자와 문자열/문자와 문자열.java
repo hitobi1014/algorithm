15bda06fade3c4b467ba5dd2d4b33aaa22352b71
Minjun Choi <66252942+hitobi1014@users.noreply.github.com>
2023-08-29 10:02:00 +0900

    [Bronze V] Title: 문자와 문자열, Time: 116 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.charAt(sc.nextInt()-1));
+    }
+
+}
