bc4e2d99ad4d7b12b4c0c415ef41a1602a54c886
minjun <hitobi1014@gmail.com>
2023-09-18 00:06:56 +0900

    IOIOI 푸는중

--- src/main/java/test/백준.java
+++ src/main/java/test/백준.java
@@ -1,69 +1,41 @@
 package test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 /**
- * depth > 1 이면 return 하기
- * 방문체크, 방문하지 않았으면 카운트 증가와 dfs 보내기
- * line1. N
- * line2. N*N char 문자
- * 필요: char[][], 답 출력 answer
+ *
  */
 public class 백준 {
-    static char[][] friends;
-    static int N;
-    static LinkedList[] list;
-    static boolean[] visited;
-
-    public static void main(String[] args) throws Exception {
-        int answer = 0;
+    static int N, M;
+    static char[] S;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        friends = new char[N][N];
-        list = new LinkedList[N];
+        M = Integer.parseInt(br.readLine());
+        S = br.readLine().toCharArray();
 
+        int lt = 0;
+        int answer = 0;
+        A:while ((lt + (N * 2) + 1) < M) {
 
-        for (int i = 0; i < N; i++) {
-            list[i] = new LinkedList<Integer>();
-        }
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                char x = str.charAt(j);
-                if (x == 'Y') {
-                    list[i].add(j);
-                }
+            if (S[lt] != 'I') {
+                lt++;
+                continue;
             }
-        }
 
-        for (int i = 0; i < list.length; i++) {
-            if (list[i].size() > 0) {
-                visited = new boolean[N];
-                visited[i] = true;
-                answer = Math.max(answer, dfs(i, 0, 0));
+            // 탐색문자열 만큼 for문
+            for (int i = 0; i < (N * 2) + 1; i++) {
+                if (i%2==0 && S[lt+i]=='I') continue;
+                if (i%2==1 && S[lt+i]=='O') continue;
+                lt++;
+                continue A;
             }
+
+            // 탐색문자열이 다 맞았을경우 cnt 증가와 lt를 N만큼 증가
+            answer++;
+            lt = N % 2 == 0 ? lt + N : lt + N + 1;
         }
         System.out.println(answer);
-    }
-
-    private static int dfs(int idx, int depth, int accrue) {
-        if (depth > 1) {
-            return accrue;
-        }
-        LinkedList getList = list[idx];
-
-        for (int i = 0; i < getList.size(); i++) {
-            int curIdx = (int) getList.get(i);
-            if(visited[curIdx]) continue;
-            visited[curIdx] = true;
-//            visited[idx] = false;
-            accrue = dfs(curIdx, depth + 1, accrue + 1);
-//            visited[curIdx] = false;
-        }
-
-        return accrue;
     }
 }
