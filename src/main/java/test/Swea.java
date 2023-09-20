ecfa21ff329889bd23b7a9bb06b76200f0ccebb5
minjun <hitobi1014@gmail.com>
2023-09-20 18:00:27 +0900

    4008 푸는중

--- src/main/java/test/Swea.java
+++ src/main/java/test/Swea.java
@@ -4,91 +4,110 @@ import java.io.*;
 import java.util.*;
 
 /**
- * N*N 배열
- * 0 지나갈수있음
- * 1 장애물
- * 2 : 2초 주기 장애물 건너려면 2초기다림
+ * line1. tc 개수
+ * line2. 숫자 개수 N
+ * line3. 연산 개수 => + - * / (순서대로 작성)
+ * line4. N개의 숫자
  */
 public class Swea {
-    static int N;
-    static int[][] map, temp;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static boolean[][] visited;
+    static int N, minN, maxN;
+    static char[] ops, temp;
+    static int[] numArr;
+    static Deque<Integer> queue;
     public static void main(String[] args) throws Exception{
-        // line1. 테케
-        // line2. N 크기
-        // line3. N개줄 배열값
-        // line4. 시작위치 A,B
-        // line5. 도착위치 C,D
         System.setIn(new FileInputStream("D:\\MJ\\algorithm\\src\\main\\java\\test\\text.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
-        for (int i=1; i<=tc; i++){
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N]; // 원본 배열
-            temp = new int[N][N]; // 걸리는 시간 담을 배열
-            for (int k = 0; k < N; k++) {
-                Arrays.fill(temp[k], Integer.MAX_VALUE);
-            }
-            visited = new boolean[N][N]; // 걸리는 시간 담을 배열
-            StringTokenizer st = null;
-            // 입력
-            for (int lc=0; lc<N; lc++){
-                st = new StringTokenizer(br.readLine());
-                for (int rc = 0; rc < N; rc++) {
-                    map[lc][rc] = Integer.parseInt(st.nextToken());
+//        StringBuilder sb = new StringBuilder();
+
+        for (int t=1; t<=tc; t++){
+            N = Integer.parseInt(br.readLine()); // 숫자 개수
+            StringTokenizer calcSt = new StringTokenizer(br.readLine());
+            ops = new char[N-1]; // 연산자 개수
+            temp = new char[N-1]; // 연산자 개수
+            numArr = new int[N];
+            queue = new ArrayDeque<>();
+
+            int idx = 0;
+            int cIdx = 0;
+            while (calcSt.hasMoreTokens()){
+                int num = Integer.parseInt(calcSt.nextToken());
+                char c = ' ';
+                if (idx == 0){
+                    c = '+';
+                } else if (idx == 1){
+                    c = '-';
+                } else if (idx == 2){
+                    c = '*';
+                } else {
+                    c = '/';
                 }
+                for (int i=0; i<num; i++) {
+                    ops[cIdx++] = c;
+                }
+                idx++;
             }
-            st = new StringTokenizer(br.readLine());
-            int sLc = Integer.parseInt(st.nextToken());
-            int sRc = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            int eLc = Integer.parseInt(st.nextToken());
-            int eRc = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            // 숫자 입력
+            for (int i=0; i<N; i++){
+                numArr[i] = Integer.parseInt(st.nextToken());
+//                queue.offer(Integer.parseInt(st.nextToken()));
+            }
+            // 로직처리
+            minN = Integer.MAX_VALUE;
+            maxN = Integer.MIN_VALUE;
 
-            bfs(sLc, sRc);
-            System.out.printf("#%d %d\n", i, temp[eLc][eRc]);
-
+            boolean[] visited = new boolean[N-1];
+            permutation(visited, 0);
+            System.out.printf("#%d %d\n", t, (maxN - minN));
+//            sb.append('#').append(t).append(maxN-minN).append('\n');
         }
+
+//        System.out.println(sb.toString());
     }
 
-    private static void bfs(int lc, int rc){
-        // 시작위치에서
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{lc, rc, 0}); //0:row 1:col 2:time
-        visited[lc][rc] = true;
+    private static void permutation(boolean[] visited, int cnt){
+        if (cnt == N-1) {
+            // queue 입력
+            for (int x : numArr) queue.offer(x);
 
-        while(!queue.isEmpty()){
-            int[] poll = queue.poll();
-            int curRow = poll[0];
-            int curCol = poll[1];
-            int time = poll[2];
+            // 숫자 2개 poll => 연산 수행 => firstoffer
+            for (int i=0; i<temp.length; i++){
+                int a = queue.poll();
+                int b = queue.poll();
 
-            // 4방 탐색, 1은 못가고 2는 걸리는시간 +2초
-            for (int i=0; i<dx.length;i++){
-                int nextRow = curRow + dx[i];
-                int nextCol = curCol + dy[i];
-
-                // nR, nC가 map 범위를 벗어나거나 장애물(1)을 만나면 continue
-                if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N
-                    || map[nextRow][nextCol] == 1 || visited[nextRow][nextCol]) continue;
-
-                // 소용돌이 만나면 +2, 아니면 1 증가
-                if (map[nextRow][nextCol] == 2) {
-                    time += 2;
-                } else {
-                    time += 1;
+                switch (temp[i]) {
+                    case '+':
+                        queue.offerFirst(a+b);
+                        break;
+                    case '-':
+                        queue.offerFirst(a-b);
+                        break;
+                    case '*':
+                        queue.offerFirst(a*b);
+                        break;
+                    case '/':
+                        queue.offerFirst(a/b);
+                        break;
                 }
-
-                // 최소 시간
-                int minTime = Math.min(temp[nextRow][nextCol], time);
-                temp[nextRow][nextCol] = minTime;
-                queue.offer(new int[]{nextRow, nextCol, minTime});
-                visited[nextRow][nextCol] = true;
-
             }
+
+            int poll = queue.poll();
+            minN = Math.min(minN, poll);
+            maxN = Math.max(maxN, poll);
+            return;
+        }
+
+        // 같은 부호에 대해 중복 순열 처리
+        for (int i = 0; i < N - 1; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            temp[cnt] = ops[i];
+
+            permutation(visited, cnt+1);
+
+            visited[i] = false;
         }
     }
 }
