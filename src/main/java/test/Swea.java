41610fad855905cb3fd71ded2d422b742161b708
minjun <dailymaro@naver.com>
2023-10-05 17:58:23 +0900

    벽돌깨기

--- src/main/java/test/Swea.java
+++ src/main/java/test/Swea.java
@@ -2,32 +2,63 @@ package test;
 
 import java.io.*;
 import java.util.*;
+import java.util.function.*;
 
 /**
  */
 public class Swea {
-    static int t, n; // 테케, 편의점수
+    static int[] selected;
+    static int N,W,H;
+    static int[][] map;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        t = Integer.parseInt(br.readLine());
-        n = Integer.parseInt(br.readLine());
+        Function<String, Integer> sToN = Integer::parseInt;
+        int testCase = sToN.apply(br.readLine());
 
-        /**
-         * n+2만큼 배열 선언
-         * 해당 배열을 0~ n+2, 각 좌표별로 차이를 할당 (x+y값의 차이)
-         * ex) 입력이
-         * 0 0
-         * 1000 0
-         * 1000 1000
-         * 2000 1000
-         *    0     1     2     3
-         * 0  0     1000  2000  3000
-         * 1  1000  0     1000  2000
-         * 2  2000  1000  0     1000
-         * 3  3000  2000  1000   0
-         *
-         * 플로이드 워셜을 활용해서 모든 정점에 대해 차이
-         * if Math.Min(  abs(map[i][k] - map[k][j]) , map[i][j])
-         */
+        for (int tc = 1; tc <= testCase; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = sToN.apply(st.nextToken()); // 구슬 개수
+            W = sToN.apply(st.nextToken()); // 너비
+            H = sToN.apply(st.nextToken()); // 높이
+
+            map = new int[H][W];
+
+            // 중복 순열 => 너비 idx
+
+            int answer = 0;
+            System.out.printf("%d %d", tc, answer);
+        }
+
+    }
+
+    // 구슬 쏘기 idx 중복순열만들기
+    static void permutation(int cnt) {
+        // 구슬개수
+        if (cnt == N) {
+
+            int[][] mapCopy = map.clone();
+
+            // 선택된 idx 차례로 쏘기
+            for (int i = 0; i < cnt; i++) {
+                int curIdx = selected[i];
+                shoot(curIdx, mapCopy);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < W; i++) {
+            selected[cnt] = i;
+            permutation(cnt + 1);
+        }
+
+    }
+
+    static void shoot(int curIdx, int[][] copy) {
+        for (int i = 0; i < H; i++) {
+            if (copy[i][curIdx] == 0) continue;
+        }
     }
 }
