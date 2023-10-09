fc6bb8d4d24d06b4735c99dce51be3165140b61a
Minjun Choi <66252942+hitobi1014@users.noreply.github.com>
2023-10-09 23:16:29 +0900

    [Gold III] Title: 줄 세우기, Time: 432 ms, Memory: 45868 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,68 +1,67 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.util.function.Function;
 
 public class Main {
-    static int N, M, students[];
-    static List[] vertexList;
-    static boolean[] visited;
-    public static void main(String[] args) throws Exception{
+    static int N, M;
+    static List[] list;
+    static int[] depth;
+    static int[] answer;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Function<String, Integer> sToN = Integer::parseInt;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        // N:학생수 , M:비교한 횟수
-        N = sToN.apply(st.nextToken());
-        M = sToN.apply(st.nextToken());
-
-        // M개줄 A, B => A<B (A는 B보다 작음)  .. 학생 번호 1~N
-        vertexList = new List[N+1];
-        visited = new boolean[N + 1];
-        students = new int[N + 1];
-
-        for (int i = 0; i < vertexList.length; i++) {
-            vertexList[i] = new ArrayList<Integer>();
+        list = new List[N+1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<Integer>();
         }
 
+        depth = new int[N+1];
+        answer = new int[N+1];
+
+        // 줄 입력
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int A = sToN.apply(st.nextToken());
-            int B = sToN.apply(st.nextToken());
-            students[B] += 1;
-            vertexList[A].add(B);
-        }
-        bfs();
+            int cur = Integer.parseInt(st.nextToken());
+            int next = Integer.parseInt(st.nextToken());
 
-        System.out.println();
+            list[cur].add(next);
+            depth[next]++; // 진입차수 증가
+        }
+
+        bfs();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(answer[i]).append(' ');
+        }
+        System.out.println(sb);
+
     }
 
-    private static void bfs() {
-        StringBuilder sb = new StringBuilder();
-        Queue<Integer> queue = new ArrayDeque<>();
-        int start = 0;
-        for (int i = 1; i < students.length; i++) {
-            if(students[i] == 0){
-                queue.offer(i);
-                visited[i] = true;
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        int idx = 1;
+        // 진입차수가 0인것들 q에 input
+        for (int i = 1; i <= N; i++) {
+            if (depth[i] == 0) {
+                q.offer(i);
             }
         }
 
-        while (!queue.isEmpty()) {
-            int curVertex = queue.poll();
-            sb.append(curVertex).append(' ');
 
-            ArrayList<Integer> getList = (ArrayList<Integer>) vertexList[curVertex];
-            for (Integer val : getList) {
-                students[val] -= 1; // 차수 -1
-                if (students[val] == 0) {
-                    queue.offer(val);
-                    visited[val] = true;
+        while (!q.isEmpty()) {
+            int curIdx = q.poll();
+            ArrayList<Integer> getList = (ArrayList<Integer>) list[curIdx];
+            answer[idx++] = curIdx;
+
+            for (int i = 0; i < getList.size(); i++) {
+                int nextIdx = getList.get(i);
+                depth[nextIdx]--;
+                if (depth[nextIdx] == 0) {
+                    q.offer(nextIdx);
                 }
             }
         }
-        System.out.println(sb.toString());
     }
-
 }
