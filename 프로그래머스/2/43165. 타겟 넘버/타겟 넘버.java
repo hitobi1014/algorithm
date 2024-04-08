992e8735359f1c9c219c3bbec8f36476b6ae7eda
Minjun Choi <66252942+hitobi1014@users.noreply.github.com>
2024-04-08 20:56:19 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+                
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    static void dfs(int[] numbers,int target,int idx, int sum) {
+        if (idx == numbers.length) {
+            if (sum == target) {
+               answer++;
+            }
+        } else {
+            dfs(numbers, target, idx+1, sum + numbers[idx]);
+            dfs(numbers, target, idx+1, sum - numbers[idx]);
+        }
+    
+    }
+}
