544522eaba31804b9e5bfd3a9bf06e447f89e66c
Minjun Choi <hitobi1014@gmail.com>
2025-01-09 23:43:37 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,52 +1,48 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int comCnt, netWork;
-    static List[] list;
-    static Set<Integer> answer;
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        comCnt = Integer.parseInt(br.readLine());
-        netWork = Integer.parseInt(br.readLine());
-
-        list = new List[comCnt+1];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = new ArrayList<Integer>();
-        }
-
-        for (int i=0; i<netWork;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list[a].add(b);
-            list[b].add(a);
-        }
-        answer = new HashSet<>();
-        answer.add(1);
-
-        solution();
-        System.out.println(answer.size()-1);
-
-    }
-
-    static void solution() {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(1);
-
-        while (!queue.isEmpty()){
-            int idx = queue.poll();
-            ArrayList<Integer> getList = (ArrayList) list[idx];
-            for (int i = 0; i < getList.size(); i++) {
-                int x = getList.get(i);
-                if (answer.contains(x)) continue;
-                answer.add(x);
-                queue.offer(x);
-            }
-        }
-    }
+  public static void main(String args[]) throws Exception{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int N = Integer.parseInt(br.readLine());
+      List[] list = new List[N+1];
+      int[] answer = new int[N+1];
+      for (int i=1; i<=N; i++) {
+          list[i] = new ArrayList<Integer>();
+          answer[i] = 1;
+      }
+      
+      int n = Integer.parseInt(br.readLine());
+      
+      for (int i=0; i<n; i++){
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          int prev = Integer.parseInt(st.nextToken());
+          int next = Integer.parseInt(st.nextToken());
+          
+          list[prev].add(next);
+          list[next].add(prev);
+      }
+      
+      answer[1] = 0;
+      
+      Queue<Integer> q = new LinkedList<>();
+      
+      q.offer(1);
+      int cnt = 0;
+      
+      while (!q.isEmpty()) {
+          int cur = q.poll();
+          
+          for (int i=0; i<list[cur].size(); i++) {
+              int x = (Integer) list[cur].get(i);
+              if (answer[x] == 0) continue;
+              q.offer(x);
+              cnt++;
+              answer[x] = 0;
+          }
+      }
+      
+      System.out.println(cnt);
+  }
 }
